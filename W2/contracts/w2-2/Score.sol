e26ddb152c135cbc68cf428ef331feef46b95d8f
luckyliang <1037895153@qq.com>
2022-03-06 22:24:55 +0800

    分数判断

--- W2/contracts/w2-2/Score.sol
+++ W2/contracts/w2-2/Score.sol
@@ -10,12 +10,17 @@ contract Score is IScore, TeacherRole {
     event AddStudentScore(uint32 indexed studentNo, uint8 score);
     event ChangeStudentScore(uint32 indexed studentNo, uint8 score);
 
-    function addStudentScore(uint32 studentNo, uint8 score) external override onlyTeacher() {
+    modifier scoreVerify(uint8 score) {
+        require(score >= 0 && score <= 100, "score illegal");
+        _;
+    }
+
+    function addStudentScore(uint32 studentNo, uint8 score) external override onlyTeacher() scoreVerify(score) {
         _studentScore[studentNo] = score;
         emit AddStudentScore(studentNo, score);
     }
 
-    function changeStudentScore(uint32 studentNo, uint8 score) external override onlyTeacher() {
+    function changeStudentScore(uint32 studentNo, uint8 score) external override onlyTeacher() scoreVerify(score) {
         _studentScore[studentNo] = score;
         emit ChangeStudentScore(studentNo, score);
     }
--- W2/contracts/w2-2/Teacher.sol
+++ W2/contracts/w2-2/Teacher.sol
@@ -5,19 +5,25 @@ import "./TeacherRole.sol";
 import "./IScore.sol";
 
 contract Teacher is TeacherRole {
-
     IScore private _scoreContract;
 
     constructor(address scoreAddress) {
         _scoreContract = IScore(scoreAddress);
     }
 
-    function addStudentScore(uint32 student, uint8 score) external  onlyTeacher() {
+    modifier scoreVerify(uint8 score) {
+        require(score >= 0 && score <= 100, "score illegal");
+        _;
+    }
+
+    function addStudentScore(uint32 student, uint8 score) external onlyTeacher() scoreVerify(score) {
         _scoreContract.addStudentScore(student, score);
     }
 
-    function changeStudentScore(uint32 student, uint8 score) external  onlyTeacher() {
+    function changeStudentScore(uint32 student, uint8 score)
+        external
+        onlyTeacher() scoreVerify(score)
+    {
         _scoreContract.changeStudentScore(student, score);
     }
-
 }
